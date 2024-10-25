6a281eaaa024ef5100d05f4c79b662b4664eea00
dienebsouza <dihcontatos@gmail.com>
2024-10-25 08:08:00 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Aula2709
+# aulasexta2709
  
---
+++ src/br/com/controle/Questoes.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.controle;
+
+/**
+ *
+ * @author laboratorio
+ */
+public class Questoes {
+    private int soma;
+
+    public int getSoma() {
+        return soma;
+    }
+
+    public void setSoma(int soma) {
+        this.soma = soma;
+        
+        this.soma++;
+    }  
+}
